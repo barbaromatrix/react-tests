31dcc4a68b73204af25e64ccc04a97e12602f793
Matheus Galdino <matheus.galdino@hotmail.com>
2018-03-06 14:38:15 -0300

    Passing selected number to status

--- App.js
+++ App.js
@@ -5,72 +5,89 @@ import _ from 'lodash';
 const Stars = (props) => {
   const numberOfStars = 1 + Math.floor(Math.random() * 9);
   return (
-  	<div className='col-md-5'>
-  	  {
-      	_.range(numberOfStars).map(i =>
-      		<i key={i} className="fa fa-star"></i>  
-      )}
-  	</div>
+    <div className='col-md-5'>
+      {
+        _.range(numberOfStars).map(i =>
+          <i key={i} className="fa fa-star"></i>
+        )
+      }
+    </div>
   );
 };
 
 const Button = (props) => {
-	return (
-  	<div className='col-md-2'>
-  	  <button>=</button>
-  	</div>
+  return (
+    <div className='col-md-2'>
+      <button>=</button>
+    </div>
   );
 };
 
 const Answer = (props) => {
-	return (
-  	<div className='col-md-5'>
-  	  <span>5</span>
-      <span>6</span>
-  	</div>
+  return (
+    <div className='col-md-5'>
+      {
+        props.selectedNumbers.map((number, i) => 
+          <span key={i}>{number}</span>
+        )
+      }
+    </div>
   );
 };
 
 const Number = (props) => {
-	return (
-  	<div className="card text-center">
-  	  <div>
-      	{
-        	Number.list.map((number, index) => 
-          	<span key={index}>{number}</span>
+  const numberClassName = (number) => {
+    if (props.selectedNumbers.indexOf(number) >= 0) {
+    	return 'selected';
+    }
+  }
+
+  return (
+    <div className="card text-center">
+      <div>
+        {
+          Number.list.map((number, index) =>
+            <span key={index} className={numberClassName(number)}>{number}</span>
           )
         }
-  	    <span className='used'>1</span>
-  	  </div>
-  	</div>
-	);
+      </div>
+    </div>
+  );
 };
-Number.list = _.range(1,10);
+Number.list = _.range(1, 10);
 
 class Game extends React.Component {
-	render(){
-  	return (
-    	<div>
-    	  <h3>Play nine</h3>
+  constructor() {
+    super();
+
+    this.state = {
+      selectedNumbers: []
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Play nine</h3>
         <hr />
         <div className="row">
           <Stars />
           <Button />
-          <Answer />
+          <Answer selectedNumbers={this.state.selectedNumbers} />
         </div>
         <br />
-        <Number />
-    	</div>
+        <Number selectedNumbers={this.state.selectedNumbers}/>
+      </div>
     );
   }
 }
 
 class App extends React.Component {
-	render(){
-  	return (
-    	<div>
-    	  <Game />
-    	</div>
+  render() {
+    return (
+      <div>
+        <Game />
+      </div>
     );
   }
 }
