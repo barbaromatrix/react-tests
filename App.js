08d99ca0225c7f43b5f29bafb5deb3d7571a9830
Matheus Galdino <matheus.galdino@hotmail.com>
2018-03-06 13:40:37 -0300

    Starting game design

--- App.js
+++ App.js
@@ -1,22 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 
-class App extends React.Component {
-	render() {
-  	return(
-    	<div>
-      	<p>This is just the master file.</p>
-        <p>To check the other examples, switch the branches</p>
-      </div>
-    );
-  }
+const Stars = (props) => {
+  const numberOfStars = 1 + Math.floor(Math.random() * 9);
+  return (
+  	<div className='col-md-5'>
+  	  {
+      	_.range(numberOfStars).map(i =>
+      		<i key={i} className="fa fa-star"></i>  
+      )}
+  	</div>
+  );
 };
 
-ReactDOM.render(
-	<App />,
-    document.getElementById('container')
-);
+const Button = (props) => {
+	return (
+  	<div className='col-md-2'>
+  	  <button>=</button>
+  	</div>
+  );
+};
 
-if (module.hot) {
-  module.hot.accept();
+const Answer = (props) => {
+	return (
+  	<div className='col-md-5'>
+  	  <span>5</span>
+      <span>6</span>
+  	</div>
+  );
+};
+
+const Number = (props) => {
+	return (
+  	<div className="card text-center">
+  	  <div>
+      	{
+        	Number.list.map((number, index) => 
+          	<span key={index}>{number}</span>
+          )
+        }
+  	    <span className='used'>1</span>
+  	  </div>
+  	</div>
+	);
+};
+Number.list = _.range(1,10);
+
+class Game extends React.Component {
+	render(){
+  	return (
+    	<div>
+    	  <h3>Play nine</h3>
+        <hr />
+        <div className="row">
+          <Stars />
+          <Button />
+          <Answer />
+        </div>
+        <br />
+        <Number />
+    	</div>
+    );
+  }
 }
+
+class App extends React.Component {
+	render(){
+  	return (
+    	<div>
+    	  <Game />
+    	</div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
